663449f0514f02051983f5c3ef9235e5d0bdb5f6
Rajan <walrajan@gmail.com>
2017-11-10 01:05:30 -0500

    adding docker readme and a link to download prebuilt docker image

--- docker/init.sh
+++ docker/init.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 echo "en_US.UTF-8 UTF-8" >> etc/locale.gen
+locale-gen
 git clone git://github.com/rjnw/hakaru-benchmarks.git
 cd /hakaru-benchmarks
 git clone git://github.com/hakaru-dev/hakaru
