df735f22e3489b0ba7e1a51cb4616a174d4ae436
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2017-11-11 15:26:57 -0500

    Fix init for new setup

--- docker/init.sh
+++ docker/init.sh
@@ -1,10 +1,8 @@
 #!/bin/zsh
 echo "en_US.UTF-8 UTF-8" >> etc/locale.gen
 locale-gen
-git pull --rebase git://github.com/rjnw/hakaru-benchmarks.git
 cd /hakaru-benchmarks
-git pull --rebase git://github.com/hakaru-dev/hakaru
-git pull --rebase git://github.com/rjnw/sham
-git pull --rebase git://github.com/rjnw/hakaru-rktjit hakrit
+git pull --rebase
+git submodule update
 make setup
 /bin/zsh
