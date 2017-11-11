46604bd7d15f3cdff77865e81e188333478c3d4f
Rajan <walrajan@gmail.com>
2017-11-11 14:52:25 -0500

    dockerchanges

--- docker/init.sh
+++ docker/init.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 echo "en_US.UTF-8 UTF-8" >> etc/locale.gen
 locale-gen
-git clone git://github.com/rjnw/hakaru-benchmarks.git
+git pull --rebase git://github.com/rjnw/hakaru-benchmarks.git
 cd /hakaru-benchmarks
-git clone git://github.com/hakaru-dev/hakaru
-git clone git://github.com/rjnw/sham
-git clone git://github.com/rjnw/hakaru-rktjit hakrit
+git pull --rebase git://github.com/hakaru-dev/hakaru
+git pull --rebase git://github.com/rjnw/sham
+git pull --rebase git://github.com/rjnw/hakaru-rktjit hakrit
 make setup
 /bin/zsh
