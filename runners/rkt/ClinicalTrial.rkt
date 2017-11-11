65c0b999f8b423a90c02d603a5b92b557627e88d
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2017-11-11 16:16:04 -0500

    Improve runner

--- runners/rkt/ClinicalTrial.rkt
+++ runners/rkt/ClinicalTrial.rkt
@@ -2,9 +2,11 @@
 
 (require sham
          hakrit
-         ffi/unsafe)
+         ffi/unsafe racket/cmdline
+        racket/runtime-path)
 
-(define module-env (compile-file "../../testcode/hkrkt/ClinicalTrial.hkr"))
+(define-runtime-path CT.hkr "../../testcode/hkrkt/ClinicalTrial.hkr")
+(define module-env (debug-file CT.hkr))
 
 (define init-rng (jit-get-function 'init-rng module-env))
 (init-rng)
@@ -13,12 +15,9 @@
 (define make-boolarr-pair (jit-get-function 'make$pair<array<bool>*.array<bool>*> module-env))
 (define make-bool-array (jit-get-function 'make$array<bool> module-env))
 
-;(printf (current-command-line-arguments))
-
-(define n 10)     ;(string->number (get-cmd-argument 0)))
-(define xfile (format "../../input/clinicalTrial/~a" n))
-
-(define xdata (file->lines (string->path xfile)))
+(define n (command-line #:args (n) (string->number n)))
+(define-runtime-path xfile "../../input/clinicalTrial/")
+(define xdata (file->lines (build-path xfile (number->string n))))
 
 (define (tobool str)
   (match str
@@ -36,18 +35,18 @@
   (define a (map tobool (regexp-split "," (second m1))))
   (define b (map tobool (regexp-split "," (third m1))))
   (define i (tobool (fourth m1)))
-  (printf "a: ~a, b: ~a, i: ~a\n" a b i)
+  ;(printf "a: ~a, b: ~a, i: ~a\n" a b i)
   (define ca (make-carray make-bool-array tbool a))
   (define cb (make-carray make-bool-array tbool b))
   (define p (make-boolarr-pair ca cb))
+  (define result (prog n p))
   (unless (equal? i (prog n p))
-    (printf "wrong!\n"))
-  (if (not (equal? i (prog n p)))
-      (set! total-not-equal (+ total-not-equal 1))
-      (void)))
+         (set! total-not-equal (+ total-not-equal 1))
+         #;
+    (printf "wrong!\n")))
 
-(for ([data xdata])
-  (run-test data))
+(time (for ([data xdata])
+          (run-test data)))
 (printf "total not equal: ~a\n" total-not-equal)
 ;;its always half of total don't know if that is supposed to happen or our implementation
 ;; is wrong as there are only two outputs
