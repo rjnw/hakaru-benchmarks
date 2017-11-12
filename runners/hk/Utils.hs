9439f8171dee3af8512e3dbabcdf047f8982df1c
Praveen Narayanan <pravnar@indiana.edu>
2017-11-12 04:36:13 -0500

    Refactor some of the common runner code into Utils.hs

---
+++ runners/hk/Utils.hs
@@ -0,0 +1,84 @@
+module Utils where
+
+import qualified Data.Vector.Unboxed as U
+import           Language.Hakaru.Runtime.LogFloatPrelude
+import           Language.Hakaru.Runtime.CmdLine
+import qualified System.Random.MWC                as MWC
+import           System.Process (readProcess)
+import           Data.Time.Clock (getCurrentTime, diffUTCTime, UTCTime)
+import           Control.Monad ((>=>), forM)
+
+gibbsSweep :: MWC.GenIO
+           -> (Int -> Measure Int) -- update one dimension
+           -> U.Vector Int         -- start state
+           -> IO (U.Vector Int)    -- state after one sweep
+gibbsSweep g update zs = loop (U.length zs) zs
+    where loop :: Int -> U.Vector Int -> IO (U.Vector Int)
+          loop 0 zs = return zs
+          loop i zs = do
+            Just zNew <- unMeasure (update (i-1)) g
+            loop (i-1) (U.unsafeUpd zs [(i-1, zNew)])    
+
+data SamplerKnobs = Knobs { minSeconds :: Double
+                          , stepSeconds :: Double
+                          , minSweeps :: Int
+                          , stepSweeps :: Int }
+
+type Sampler = SamplerKnobs ->
+               IO (Double, -- initialization time in seconds
+                   [(Double, -- seconds since beginning of initialization
+                     Int, -- sweeps performed so far
+                     U.Vector Int)]) -- current classification
+
+timeHakaru :: UTCTime -- time0
+           -> (U.Vector Int -> IO (U.Vector Int)) -- sweeper function
+           -> U.Vector Int -- start state
+           -> Sampler
+timeHakaru time0 sweep zs knobs = do
+  time1 <- getCurrentTime
+  let sweeps :: Int -> U.Vector Int -> IO (U.Vector Int)
+      sweeps 0 = return
+      sweeps n = sweep >=> sweeps (n-1)
+      threshCond t i = t >= minSeconds knobs &&
+                       i >= minSweeps  knobs
+      loop :: Int -> Double -> Double -> U.Vector Int
+           -> IO [(Double, Int, U.Vector Int)]
+      loop iter time2 time2subgoal zs
+        | threshCond time2 iter = return []
+        | otherwise = do
+            zs <- sweeps (stepSweeps knobs) zs
+            time2 <- (`diffTime` time0) <$> getCurrentTime
+            iter  <- return (iter + stepSweeps knobs)
+            if time2 >= time2subgoal || threshCond time2 iter
+            then ((time2, iter, zs) : ) <$>
+                 loop iter time2 (time2 + stepSeconds knobs) zs
+            else loop iter time2 time2subgoal zs
+  samples <- loop 0 0 (stepSeconds knobs) zs
+  return (diffTime time1 time0, samples)
+  
+diffTime :: UTCTime -> UTCTime -> Double
+diffTime a b = fromRational . toRational $ diffUTCTime a b
+
+timeJags :: String -- filename containing JAGS driver
+         -> FilePath -- temp file path
+         -> Int -- how many clusters
+         -> Sampler
+timeJags driverSrc fp classes knobs = do
+  output <- readProcess "R"
+                        ["--slave", "-f", driverSrc, "--args",
+                         show classes, fp,
+                         show (minSeconds knobs),
+                         show (stepSeconds knobs),
+                         show (minSweeps knobs),
+                         show (stepSweeps knobs)]
+                        ""
+  let times:samples  = lines output
+      [time0, time1] = map read (words times)
+      parse (stats:zs_:rest) = (time2 - time0, iter, zs) : parse rest
+        where time2           = read time2_
+              iter            = read iter_
+              [time2_, iter_] = words stats
+              zs              = U.fromList . map (pred . read) . words $ zs_
+      parse _ = []
+  return (time1 - time0, parse samples)
+         
