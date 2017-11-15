ffd47906c9dda40bbea4d7441656f00a389c31dd
Praveen Narayanan <pravnar@indiana.edu>
2017-11-14 19:44:55 -0500

    Resample trials using linear interpolation

--- runners/hk/GmmGibbs/Accuracy.hs
+++ runners/hk/GmmGibbs/Accuracy.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Environment (getArgs)
 import Data.List (permutations)
 import Data.Array (accumArray, elems, assocs, (!))
+import qualified Data.Map.Strict as M
 
 import Log
 
@@ -16,7 +17,10 @@ main = do
   [inputs_path, logs_path] <- getArgs
   inputs <- readFile inputs_path
   logs   <- readFile logs_path
-  mapM_ print (zipWith (\i l -> map (process (parseInput i)) (parseTrial l))
+  let ptsPerLn = 50
+      accs = [1/ptsPerLn * i | i <- [1..ptsPerLn]]
+      processLn i l = map (process (parseInput i)) (parseTrial l)
+  mapM_ print (zipWith (\i -> resampleWith accs . processLn i)
                        (lines inputs) (lines logs))
 
 process :: Input -> Snapshot -> Snapshot
@@ -34,3 +38,33 @@ process (_,truth) (Snapshot p predict) = Snapshot p [accuracy]
 
 maxClasses :: Int
 maxClasses = 12 -- factorial 13 is probably too big anyway
+
+-- | Resample trials at regular accuracy-intervals using linear interpolation
+--------------------------------------------------------------------------------
+
+resampleWith :: [Double] -> Trial -> Trial
+resampleWith accs trial = fromSamples $ map (\a -> (a, query table a)) accs
+    where table = toTable trial
+          fromSamples :: [(Double,[Double])] -> Trial
+          fromSamples = map (\(a,p) -> Snapshot p [a])
+
+-- | Map from accuracies to (runtime:_)
+type Table = M.Map Double [Double]
+
+toTable :: Trial -> Table
+toTable = M.fromList . map (\(Snapshot p [acc]) -> (acc,p))
+
+query :: Table -> Double -> [Double]
+query table x =
+    case (M.lookupLE x table, M.lookupGE x table) of
+      (Nothing, Just (b,pb)) -> pb
+      (Just (a,pa), Nothing) -> pa
+      (Just (a,pa), Just (b,pb)) ->
+          if a == b then pa else interpolate (a,pa) (b,pb) x
+      (Nothing, Nothing) -> error "query: something went wrong"
+
+-- | Expects the y-coordinate to look like [time,nsweeps]
+interpolate :: (Double,[Double]) -> (Double,[Double]) -> Double -> [Double]
+interpolate (a,[ta,na]) (b,[tb,nb]) x = [ ta + (tb-ta)*(x-a)/(b-a)
+                                        , fromIntegral . floor $ (na + nb) / 2 ]
+
