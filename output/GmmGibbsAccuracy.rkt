9eeace742e7f9fc6f1413c179945a5421b9d9957
Rajan <walrajan@gmail.com>
2018-06-19 23:38:59 -0400

    gmmaccuracy

--- output/GmmGibbsAccuracy.rkt
+++ output/GmmGibbsAccuracy.rkt
@@ -54,7 +54,7 @@
       ;; (printf "~a " min-time)
       (map (λ (shot) (list (- (first shot) min-time) (second shot) (third shot))) trial)))
 
-  (define (line-intr runner lcolor lstyle icolor istyle legend pstyle  (i 1))
+  (define (line-intr runner lcolor lstyle icolor istyle legend pstyle  (i 1) (point-size 6))
     (define pr (remove-warmup (parse runner)))
     (define sta (run->sweep-time.accuracy pr))
     ;; (pretty-display sta)
@@ -78,11 +78,11 @@
       #:line1-style 'transparent #:line2-style 'transparent
       #:alpha 0.3)
 
-     (points
-      (for/list ([(s ta) smta]
-                 #:when (zero? (modulo s 10)))
-        ta)
-      #:size 6 #:color lcolor #:sym pstyle)
+     ;; (points
+     ;;  (for/list ([(s ta) smta]
+     ;;             #:when (zero? (modulo s 10)))
+     ;;    ta)
+     ;;  #:size point-size #:color lcolor #:sym pstyle)
      ;; (points
      ;;  (apply append
      ;;         (for/list ([trial pr])
@@ -113,32 +113,32 @@
      (append
       ;; http://www.somersault1824.com/wp-content/uploads/2015/02/color-blindness-palette.png
       ;; line-color line-style interval-color style legend line-points
-      ;; (line-intr "rkt"
-      ;;            (make-object color% 0 73 73) 'solid
-      ;;            (make-object color% 0 146 146) 'solid
-      ;;            "New LLVM-backend" 'triangle)
-
-      ;; (line-intr "hk"
-      ;;            (make-object color% 73 0 146) 'dot-dash
-      ;;            (make-object color% 0 146 146) 'solid
-      ;;            "Old Haskell-backend" 'square)
-      ;; (line-intr "jags"
-      ;;            (make-object color% 146 0 0) 'short-dash
-      ;;            (make-object color% 146 73 0) 'solid
-      ;;            "JAGS" 'diamond)
-      (line-intr "augur"
+      (line-intr "rkt"
                  (make-object color% 0 73 73) 'solid
                  (make-object color% 0 146 146) 'solid
+                 "New LLVM-backend" 'triangle)
+
+      (line-intr "hk"
+                 (make-object color% 73 0 146) 'dot-dash
+                 (make-object color% 0 146 146) 'solid
+                 "Old Haskell-backend" 'square)
+      (line-intr "jags"
+                 (make-object color% 146 0 0) 'short-dash
+                 (make-object color% 146 73 0) 'solid
+                 "JAGS" 'diamond)
+      (line-intr "augur"
+                 (make-object color% 255 109 182) 'solid
+                 (make-object color% 255 182 119) 'solid
                  "Augur" 'triangle)
       (list (tick-grid)))
-          "augur50-10000.pdf"
-     ;; (format "../../ppaml/writing/pipeline/GmmGibbs~a-~a.pdf" classes pts)
+          ;; "augur50-10000.pdf"
+     (format "../../ppaml/writing/pipeline/GmmGibbs~a-~a.pdf" classes pts)
 
-     ;; #:y-max 50
-     ;; #:y-min 0
-     ;; #:x-max 90
-     ;; #:width 500
-     ;; #:height 300
+     #:y-max 50
+     #:y-min 0
+     #:x-max 90
+     #:width 500
+     #:height 300
      #:y-label "Accuracy in %" #:x-label "Time in seconds" #:legend-anchor 'bottom-right))
   )
 
