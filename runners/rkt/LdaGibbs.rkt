d4c2633d878e695819aeaf94399d7aa8a90a338d
Rajan <walrajan@gmail.com>
2018-06-25 20:08:26 -0400

    lda running for rkt

--- runners/rkt/LdaGibbs.rkt
+++ runners/rkt/LdaGibbs.rkt
@@ -1,8 +1,8 @@
 #lang racket
 (require hakrit)
 
-(require sham
-         hakrit
+(require hakrit
+         hakrit/utils
          ffi/unsafe
          racket/cmdline
          racket/runtime-path
@@ -11,79 +11,91 @@
          "utils.rkt")
 
 (define testname "LdaGibbs")
-
-(define (run-test)
+(define news-dir "news")
+(define (run-test (num-trials 1))
   (define srcfile (build-path hksrc-dir (string-append testname ".hkr")))
-  (define nbinfo (list (list) (list) (list) (list) (list) (list) (list) (list)))
 
-  (define wordsfile  (build-path input-dir "news" "words"))
-  (define docsfile   (build-path input-dir "news" "docs"))
-  (define topicsfile   (build-path input-dir "news" "topics"))
-
-
-  (define outfile (build-path output-dir testname "rkt" "out"))
-
-  (define module-env (compile-file srcfile nbinfo))
-  ;(jit-dump-module module-env)
-  (jit-verify-module module-env)
-  (initialize-jit! module-env)
-  (define init-rng (jit-get-function 'init-rng module-env))
-  (init-rng)
-
+  (define wordsfile  (build-path input-dir news-dir "words"))
+  (define docsfile   (build-path input-dir news-dir "docs"))
+  (define topicsfile   (build-path input-dir news-dir "topics"))
 
   (define rk-words (map string->number (file->lines wordsfile)))
   (define rk-docs (map string->number (file->lines docsfile)))
   (define rk-topics (map string->number (file->lines topicsfile)))
 
-  (define make-prob-array (jit-get-function (string->symbol (format "make$array<prob>")) module-env))
-  (define set-index-prob-array (jit-get-function (string->symbol (format "set-index!$array<prob>")) module-env))
-
-  (define make-nat-array (jit-get-function (string->symbol (format "make$array<nat>")) module-env))
-  (define set-index-nat-array (jit-get-function (string->symbol (format "set-index!$array<nat>")) module-env))
-  (define get-index-nat-array (jit-get-function (string->symbol (format "get-index$array<nat>")) module-env))
-
-  (define make-real-array (jit-get-function (string->symbol (format "make$array<real>")) module-env))
-  (define set-index-real-array (jit-get-function (string->symbol (format "set-index!$array<real>" )) module-env))
-
-  (define prog (jit-get-function 'prog module-env))
-
+  (define words-size (length rk-words))
+  (define docs-size (length rk-docs))
+  (define topics-size (length rk-topics))
   (define num-docs (add1 (last rk-docs)))
   (define num-words (add1 (argmax identity rk-words)))
   (define num-topics (add1 (argmax identity rk-topics)))
 
-  (define topicPrior (make-prob-array num-topics
-                                      (list->cblock (build-list num-topics (const 0.0)) _double)))
-  (define wordPrior (make-prob-array num-words
-                                     (list->cblock (build-list num-words (const 0.0)) _double)))
+  (define outfile (build-path output-dir testname "rkt" (format "~a-~a-~a" news-dir num-topics num-docs)))
 
-  (define words (make-nat-array (length rk-words) (list->cblock rk-words _uint64)))
-  (define docs (make-nat-array (length rk-docs) (list->cblock rk-docs _uint64)))
+  (define full-info
+    `(((array-info . ((size . ,num-topics))))
+      ((array-info . ((size . ,num-words))))
+      ()
+      ;; ((nat-info . ((value . ,num-docs))))
+      ((array-info . ((size . ,words-size)
+                      (value . ,rk-words))))
+      ((array-info . ((size . ,words-size)
+                      (value . ,rk-docs))))
+      ((array-info . ((size . ,words-size))))
+      ((nat-info . ((value-range . (0 . ,(- num-words 1))))))))
 
-  (define (update z docUpdate)
-    (prog topicPrior wordPrior num-docs words docs z docUpdate))
+  (printf "compiling\n")
+  (define prog (compile-hakaru srcfile full-info))
+  (printf "compiled\n")
 
-  (define zs (replicate-uniform-discrete (length rk-words) 0 (- num-topics 1)))
-  (define z (make-nat-array (length rk-words) (list->cblock zs _uint64)))
+  (printf "num-docs: ~a, num-words: ~a, num-topics: ~a\n" num-docs num-words num-topics)
+  (printf "words-size: ~a, docs-size: ~a, topics-size: ~a\n" words-size docs-size topics-size)
 
+  (define topics-prior (list->cblock (build-list num-topics (const 0.0)) _double))
+  (printf "made topics-prior\n")
+  (define words-prior (list->cblock (build-list num-words (const 0.0)) _double))
+  (printf "made words-prior\n")
+
+  (define words (list->cblock rk-words _uint64))
+  (printf "made words\n")
+  (define docs (list->cblock rk-docs _uint64))
+  (printf "made docs\n")
+
+  (define (update z word-update)
+    (prog topics-prior words-prior num-docs words docs z word-update))
+
+  (printf "made update\n")
+
+  (define distf (discrete-sampler 0 (- num-topics 1)))
+  (define zs  (for/list ([i (in-range (length rk-words))])
+                (distf)))
+  (printf "made zs\n")
+  (define z (list->cblock zs _uint64))
+
+  ;; (printf "calling update\n")
+  ;; (printf "update: ~a\n" (update z 0))
+  ;; (error 'stop)
+  (printf "running-trial\n")
   (define (run-single out-port)
-    (gibbs-timer (curry gibbs-sweep num-topics set-index-nat-array update)
+    (gibbs-timer (curry gibbs-sweep num-topics nat-array-set! update)
                  z
                  (λ (tim sweeps state)
                    (fprintf out-port "~a ~a [" (~r tim #:precision '(= 3)) sweeps)
-                   (for ([i (in-range (- num-topics 1))])
-                     (fprintf out-port "~a, " (get-index-nat-array state i)))
-                   (fprintf out-port "~a]\t" (get-index-nat-array state (- num-topics 1)))))
-    (printf "final state: ~a, \n\tactual state: ~a\n"
-            (for/list ([i (in-range num-topics)])
-              (get-index-nat-array z i))
-            rk-topics))
+                   (for ([i (in-range (- num-words 1))])
+                     (fprintf out-port "~a, " (nat-array-ref state i)))
+                   (fprintf out-port "~a]\t" (nat-array-ref state (- num-words 1)))))
+    (fprintf out-port "\n"))
 
 
   (call-with-output-file outfile #:exists 'replace
     (λ (out-port)
-      (run-single out-port))))
+      (for ([i (in-range num-trials)])
+        (run-single out-port)))))
 
 
 
-
-(run-test)
+(module+ main
+    (define num-trials
+      (command-line #:args ((num-trials "1"))
+                    (string->number num-trials)))
+  (run-test num-trials))
