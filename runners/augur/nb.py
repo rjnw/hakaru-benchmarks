0fe97aba656ea7192b015b8d90546f184ad3dd8a
rajan <walrajan@gmail.com>
2018-07-02 11:52:19 -0400

    cleanup

--- runners/augur/nb.py
+++ runners/augur/nb.py
@@ -30,22 +30,7 @@ augur_nb = '''(K : Int, D1 : Int, D2 : Int, N1 : Int, N2 : Int, topic_prior : Ve
 # z1 topics for training documents w1 words for z1
 # z2 topics for heldout documents
 
-
-augur_nb_old = '''(K : Int, D : Int, N : Int, topic_prior : Vec Real, word_prior : Vec Real, doc : Vec Int) => {
-  param theta ~ Dirichlet(topic_prior);
-  param phi[k] ~ Dirichlet(word_prior)
-      for k <- 0 until K ;
-  param z[d] ~ Categorical(theta)
-      for d <- 0 until D ;
-  data w[n] ~ Categorical(phi[z[doc[n]]])
-      for n <- 0 until N;
-}
-'''
-  # data w[d, n] ~ Categorical(phi[z[d]])
-  #     for d <- 0 until D, n <- 0 until N[d] ;
-
-sched1 = 'ConjGibbs [theta] (*) ConjGibbs [phi] (*) DiscGibbs [z2]'
-sched2 = 'DiscGibbs [z2]'
+sched = 'ConjGibbs [theta] (*) ConjGibbs [phi] (*) DiscGibbs [z2]'
 
 def holdout(i):
     return (i%50 == 0)
@@ -104,7 +89,7 @@ def run_nb(words, docs, topics, output_dir, num_samples):
     with AugurInfer('config.yml', augur_nb) as infer_obj:
         augur_opt = AugurOpt(cached=False, target='cpu', paramScale=None)
         infer_obj.set_compile_opt(augur_opt)
-        infer_obj.set_user_sched(sched1)
+        infer_obj.set_user_sched(sched)
         init_time = time.clock()
         c = infer_obj.compile(num_topics, D1, D2, N1, N2,
                               topic_prior, word_prior, doc1, doc2)(z1, w1, w2)
@@ -121,30 +106,10 @@ def run_nb(words, docs, topics, output_dir, num_samples):
             num_samples += 1
         out.write('\n')
 
-news_dir = '../../input/news/'
-words_file=news_dir+'words'
-docs_file=news_dir+'docs'
-topics_file=news_dir+'topics'
-
 def loadNewsFile(fname) :
     with open(fname) as inp:
         raw=inp.readlines()
     return map(int, raw)
-
-def doc_word(docs, words):
-    arr = []
-    acc = []
-    curr_doc = docs[0]
-    for doc, word in zip(docs, words):
-        if doc is curr_doc:
-            acc += [word]
-        else:
-            curr_doc = doc
-            arr += [np.array(acc, dtype=np.int32)]
-            acc = [word]
-    arr+= [np.array(acc, dtype=np.int32)]
-    return np.array(arr)
-
 
 if __name__ == '__main__':
     [ns, input_folder, output_dir, num_samples] = sys.argv
@@ -158,9 +123,4 @@ if __name__ == '__main__':
     with open(output_dir + '/augur' , 'w') as out:
         run_nb(words, docs, topics, output_dir, int(num_samples))
 
-# if __name__ == '__main__':
-#     words=loadNewsFile(words_file)
-#     docs=loadNewsFile(docs_file)
-#     topics=loadNewsFile(topics_file)
-#     print 'loaded news...'
-#     run_nb(words, docs, topics, '../../output/NaiveBayesGibbs/augur/')
+# python2 nb.py ../../input/news/ ../../output/NaiveBayesGibbs/augur/ 1
