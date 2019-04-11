7ebf5708c5e7028707e26f4b66ee204f3e1343b4
rajan <wal.rajan@gmail.com>
2019-04-11 17:32:54 -0400

    naive bayes benchmarks file for fastdsl paper

---
+++ runners/rkt/NaiveBayesGibbsOptSham.rkt
@@ -0,0 +1,131 @@
+#lang racket
+(require hakrit)
+
+(require sham
+         hakrit
+         ffi/unsafe
+         racket/cmdline
+         racket/runtime-path
+         racket/date
+         disassemble
+         "discrete.rkt"
+         "gmm-utils.rkt")
+
+(define testname "NaiveBayesGibbs")
+
+(define (run-test num-trials trial-sweeps trial-time holdout-modulo)
+  (define srcfile (build-path hksrc-dir (string-append testname ".hkr")))
+  (define newsd "news")
+  (define wordsfile  (build-path input-dir newsd "words"))
+  (define docsfile   (build-path input-dir newsd "docs"))
+  (define topicsfile   (build-path input-dir newsd "topics"))
+
+  (define rk-words (map string->number (file->lines wordsfile)))
+  (define rk-docs (map string->number (file->lines docsfile)))
+  (define rk-topics (map string->number (file->lines topicsfile)))
+
+  (define words-size (length rk-words))
+  (define docs-size (length rk-docs))
+  (define topics-size (length rk-topics))
+  (define num-docs (add1 (last rk-docs)))
+  (define num-words (add1 (argmax identity rk-words)))
+  (define num-topics (add1 (argmax identity rk-topics)))
+  (printf "num-docs: ~a, num-words: ~a, num-topics: ~a\n" num-docs num-words num-topics)
+  (printf "words-size: ~a, docs-size: ~a, topics-size: ~a\n" words-size docs-size topics-size)
+
+  (define outfile (build-path output-dir testname "rkt" (format "~a-~a-~a" num-topics num-docs holdout-modulo)))
+
+  (define full-info
+    `((topic_prior . ((array-info . ((size . ,num-topics)))))
+      (word_prior . ((array-info . ((size . ,num-words)))))
+      (z . ((array-info . ((size . ,num-docs)
+                           ;; (elem-info . ((nat-info
+                           ;;            . ((value-range
+                           ;;                . (0 . ,(- num-topics 1)))))))
+                           ))))
+      (w . ((array-info . ((size . ,words-size)
+                           ;; (elem-info . ((nat-info . ((value-range . (0 . ,(- num-words 1)))))))
+                           ;; (value . ,words)
+                           ))))
+      (doc . ((array-info . ((size . ,words-size)
+                             ;; (elem-info . ((nat-info
+                             ;;                . ((value-range
+                             ;;                    . (0 . ,(- num-docs 1)))))))
+                             ;; (value . ,docs)
+                             ))))
+      ;; (docUpdate . ((nat-info . ((value-range . (0 . ,(- num-docs 1)))))))
+      ))
+
+  (for ([i (in-range 100)])
+    (define pre-time (get-time))
+    (compile-file srcfile full-info)
+    (printf "~a\n" (elasp-time pre-time)))
+  (error 'stop)
+  (define module-env (compile-file srcfile full-info))
+  (define prog (get-prog module-env))
+  (printf "compiled prog\n")
+
+  (define topicPrior (list->cblock (build-list num-topics (const 0.0)) _double))
+  (define wordPrior (list->cblock (build-list num-words (const 0.0)) _double))
+
+  (define words (list->cblock rk-words _uint64))
+  (define docs (list->cblock rk-docs _uint64))
+  (printf "loaded words, docs\n")
+  (define (holdout? i) (zero? (modulo i holdout-modulo)))
+  ;; holding out only every 10, similar to haskell
+
+  (define (update z docUpdate)
+    (if (holdout? docUpdate)
+        (begin
+          (prog topicPrior wordPrior z words docs docUpdate))
+        (fixed-hakrit-array-ref z 'nat docUpdate)))
+
+  (define distf (discrete-sampler 0 (- num-topics 1)))
+  (define zs (for/list ([orig-value rk-topics]
+                        [i (in-range (length rk-topics))])
+               (if  (holdout? i)
+                    (distf)
+                    orig-value)))
+  (define z (list->cblock zs _uint64))
+
+  (define (run out-port)
+    (define (printer tim sweeps state)
+      (printf "sweeped: ~a in ~a\n" sweeps (~r tim #:precision '(= 3)))
+      (fprintf out-port "~a ~a [" (~r tim #:precision '(= 3)) sweeps)
+      (for ([i (in-range (- num-docs 1))])
+        (fprintf out-port "~a " (fixed-hakrit-array-ref state 'nat i)))
+      (fprintf out-port "~a]\t" (fixed-hakrit-array-ref state 'nat (- num-docs 1))))
+    (define sweeper (curry gibbs-sweep num-docs (λ (arr i v) (fixed-hakrit-array-set! arr 'nat i v)) update))
+    (gibbs-timer sweeper z printer
+                 #:min-sweeps trial-sweeps
+                 #:step-sweeps 1
+                 #:min-time trial-time)
+    (fprintf out-port "\n"))
+
+  (printf "locked and loaded!\nrunning-test:\n")
+  (for ([i (in-range 100)])
+    (define pre-time (get-time))
+    (prog topicPrior wordPrior z words docs 10)
+    (print (elasp-time pre-time)) (newline))
+  ;; (call-with-output-file outfile #:exists 'replace
+  ;;   (λ (out-port)
+  ;;     (for ([i (in-range num-trials)])
+  ;;       (for ([i (in-range (length rk-topics))]
+  ;;             #:when (holdout? i))
+  ;;         (nat-array-set! z i (distf)))
+  ;;       (run out-port))))
+  )
+
+(module+ test
+  (run-test 1 10 10 1))
+
+(module+ main
+  ;; (define-values (num-trials trial-sweeps trial-time holdout-modulo)
+  ;;     (command-line #:args (num-trials trial-sweeps trial-time holdout-modulo)
+  ;;                   (values (string->number num-trials)
+  ;;                           (string->number trial-sweeps)
+  ;;                           (string->number trial-time)
+  ;;                           (string->number holdout-modulo))))
+  ;; (run-test num-trials trial-sweeps trial-time holdout-modulo)
+  (run-test 1 1 1 10)
+  )
---
+++ runners/rkt/NaiveBayesGibbsSham.rkt
@@ -0,0 +1,112 @@
+#lang racket
+(require hakrit)
+
+(require sham
+         hakrit
+         ffi/unsafe
+         racket/cmdline
+         racket/runtime-path
+         racket/date
+         disassemble
+         "discrete.rkt"
+         "gmm-utils.rkt")
+
+(define testname "NaiveBayesGibbs")
+
+(define (run-test)
+  (define srcfile (build-path hksrc-dir (string-append testname ".hkr")))
+  (define newsd "news")
+  (define wordsfile  (build-path input-dir newsd "words"))
+  (define docsfile   (build-path input-dir newsd "docs"))
+  (define topicsfile   (build-path input-dir newsd "topics"))
+
+  (define words (map string->number (file->lines wordsfile)))
+  (define docs (map string->number (file->lines docsfile)))
+  (define topics (map string->number (file->lines topicsfile)))
+
+  (define words-size (length words))
+  (define docs-size (length docs))
+  (define topics-size (length topics))
+
+  (define num-docs (add1 (last docs)))
+  (define num-words (add1 (argmax identity words)))
+  (define num-topics (add1 (argmax identity topics)))
+
+  (printf "num-docs: ~a, num-words: ~a, num-topics: ~a\n" num-docs num-words num-topics)
+  (printf "words-size: ~a, docs-size: ~a, topics-size: ~a\n" words-size docs-size topics-size)
+
+  (define outfile (build-path output-dir testname "rkt" (format "~a-~a" num-topics num-docs)))
+
+  (for ([i (in-range 100)])
+    (define pre-time (get-time))
+    (compile-file srcfile)
+    (printf "~a\n" (elasp-time pre-time)))
+  (error 'stop)
+  (define module-env (compile-file srcfile))
+  (define prog (get-prog module-env))
+
+  (define topic-prior (sized-real-array (build-list num-topics (const 0.0))))
+  (define word-prior (sized-real-array (build-list num-words (const 0.0))))
+
+  (define c-words (sized-nat-array words))
+  (define c-docs (sized-nat-array docs))
+
+  (define holdout-modulo 10)
+  (define (holdout? i) (zero? (modulo i holdout-modulo)))
+  ;; holding out only every 10, similar to haskell
+
+  (define (update z doc-update)
+    (if (holdout? doc-update)
+        (let ([newTopic (prog topic-prior word-prior z c-words c-docs doc-update)])
+          newTopic)
+        (sized-nat-array-ref z doc-update)))
+
+  (define distf (discrete-sampler 0 (- num-topics 1)))
+  (define zs (for/list ([orig-value topics]
+                        [i (in-range (length topics))])
+               (if (holdout? i) (distf) orig-value)))
+  (define z (sized-nat-array zs))
+
+  (define (accuracy)
+    (define-values (correct total)
+      (for/fold ([correct '()]
+                 [total '()])
+                ([i (in-range num-docs)]
+                 [orig-value  topics]
+                 #:when (holdout? i))
+        (values (if (equal? orig-value (sized-nat-array-ref z i))
+                    (cons i correct)
+                    correct)
+                (cons i total))))
+    ;; (printf "correct: ~a\n" correct)
+    (printf "\naccuracy: ~a/~a\n" (length correct) (length total))
+    (* (/ (* (length correct) 1.0) (length total)) 100.0))
+  (define (run out-port)
+    (gibbs-timer (curry gibbs-sweep num-docs sized-nat-array-set! update)
+                 z
+                 (λ (tim sweeps state)
+                   (printf "printing\n")
+                   (fprintf out-port "~a ~a [" (~r tim #:precision '(= 3)) sweeps)
+                   (for ([i (in-range (- num-docs 1))])
+                     (fprintf out-port "~a, " (sized-nat-array-ref state i)))
+                   (fprintf out-port "~a]\t" (sized-nat-array-ref state (- num-topics 1))))
+                 #:min-sweeps 1
+                 #:step-sweeps 1
+                 #:min-time 0))
+  (printf "locked and loaded!\nrunning-test:\n")
+  (for ([i (in-range 100)])
+    (define pre-time (get-time))
+    (prog topic-prior word-prior z c-words c-docs 10)
+    (print (elasp-time pre-time)) (newline))
+  ;; (call-with-output-file outfile #:exists 'replace
+  ;;   (λ (out-port)
+  ;;     (printf "starting at: ~a\n" (current-date))
+  ;;     (run out-port)
+  ;;     (printf "finished at: ~a\n" (current-date))))
+  )
+
+(module+ test
+  (run-test))
+
+(module+ main
+  (run-test))
